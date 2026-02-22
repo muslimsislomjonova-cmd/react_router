1070faf175a0048854b9efb583f1eae3ff73515a
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2026-02-22 15:38:45 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import { useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./style.css";
+
+function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const controlledLogin = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      toast.warning("bosh maydon");
+      return;
+    }
+
+    try {
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          password,
+          expiresInMins: 30,
+        }),
+      });
+
+      if (!res.ok) throw new Error();
+
+      const data = await res.json();
+      console.log("Controlled:", data);
+
+      toast.success("successful ");
+    } catch {
+      toast.error("error");
+    }
+  };
+
+
+  const userRef = useRef();
+  const passRef = useRef();
+
+  const uncontrolledLogin = async (e) => {
+    e.preventDefault();
+
+    const user = userRef.current.value;
+    const pass = passRef.current.value;
+
+    if (!user || !pass) {
+      toast.warning("bosh maydon");
+      return;
+    }
+
+    try {
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: user,
+          password: pass,
+          expiresInMins: 30,
+        }),
+      });
+
+      if (!res.ok) throw new Error();
+
+      const data = await res.json();
+      console.log("uncontrolled:", data);
+
+      toast.success("successful");
+    } catch {
+      toast.error("error ");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>User Login</h1>
+
+      
+      <div className="card">
+        <h2>Controlled Login</h2>
+        <form onSubmit={controlledLogin}>
+          <input
+            type="text" placeholder="emilys" value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password" placeholder="emilyspass" value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+
+      
+      <div className="card">
+        <h2>Uncontrolled Login</h2>
+        <form onSubmit={uncontrolledLogin}>
+          <input type="text" placeholder="emilys" ref={userRef} />
+          <input type="password" placeholder="emilyspass" ref={passRef} />
+          <button>Login</button>
+        </form>
+      </div>
+
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
+  );
+}
+
+export default App;
